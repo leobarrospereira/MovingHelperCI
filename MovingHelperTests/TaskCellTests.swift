af66cc3ed71d1f7cf79e7829e77c83265b317c61
Leonardo Barros <leonardo.barrospereira@gmail.com>
2015-09-12 12:57:41 -0300

    Update grabbing cell for test

--- MovingHelperTests/TaskCellTests.swift
+++ MovingHelperTests/TaskCellTests.swift
@@ -13,36 +13,55 @@ import MovingHelper
 class TaskCellTests: XCTestCase {
 
     func testCheckingCheckboxMarksTaskDone() {
-        let cell = TaskTableViewCell()
+        var testCell: TaskTableViewCell?
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
         
-        let expectation = expectationWithDescription("Task updated")
-        
-        struct TestDelegate: TaskUpdatedDelegate {
-            let testExpectation: XCTestExpectation
-            let expectedDone: Bool
-            
-            init(updatedExpectation: XCTestExpectation, expectedDoneStateAfterToggle: Bool) {
-                testExpectation = updatedExpectation
-                expectedDone = expectedDoneStateAfterToggle
-            }
-            
-            func taskUpdated(task: Task) {
-                XCTAssertEqual(expectedDone, task.done, "Task done state did not match expected!")
-                testExpectation.fulfill()
-            }
+        if let navVC = mainStoryboard.instantiateInitialViewController() as? UINavigationController,
+            listVC = navVC.topViewController as? MasterViewController {
+                
+                let tasks = TaskLoader.loadStockTasks()
+                listVC.createdMovingTasks(tasks)
+                testCell = listVC.tableView(listVC.tableView, cellForRowAtIndexPath: NSIndexPath(forRow: 0,
+                    inSection: 0)) as? TaskTableViewCell
+                
+                if let cell = testCell {
+                    let cell = TaskTableViewCell()
+                    let expectation = expectationWithDescription("Task updated")
+                    
+                    struct TestDelegate: TaskUpdatedDelegate {
+                        let testExpectation: XCTestExpectation
+                        let expectedDone: Bool
+                        
+                        init(updatedExpectation: XCTestExpectation, expectedDoneStateAfterToggle: Bool) {
+                            testExpectation = updatedExpectation
+                            expectedDone = expectedDoneStateAfterToggle
+                        }
+                        
+                        func taskUpdated(task: Task) {
+                            XCTAssertEqual(expectedDone, task.done, "Task done state did not match expected!")
+                            testExpectation.fulfill()
+                        }
+                    }
+                    
+                    let testTask = Task(aTitle: "TestTask", aDueDate: .OneMonthAfter)
+                    XCTAssertFalse(testTask.done, "Newly created task is already done!")
+                    cell.delegate = TestDelegate(updatedExpectation: expectation, expectedDoneStateAfterToggle: true)
+                    cell.configureForTask(testTask)
+                    
+                    XCTAssertFalse(cell.checkbox.isChecked, "Checkbox checked for not-done task!")
+                    
+                    cell.checkbox.sendActionsForControlEvents(.TouchUpInside)
+                    
+                    XCTAssertTrue(cell.checkbox.isChecked, "Checkbox not checked after tap!")
+                    waitForExpectationsWithTimeout(2, handler: nil)
+                    
+                } else {
+                    XCTFail("Test cell was nil!")
+                }
+                
+        } else {
+            XCTFail("Could not get reference to list VC!")
         }
-        
-        let testTask = Task(aTitle: "TestTask", aDueDate: .OneMonthAfter)
-        XCTAssertFalse(testTask.done, "Newly created task is already done!")
-        cell.delegate = TestDelegate(updatedExpectation: expectation, expectedDoneStateAfterToggle: true)
-        cell.configureForTask(testTask)
-        
-        XCTAssertFalse(cell.checkbox.isChecked, "Checkbox checked for not-done task!")
-        
-        cell.checkbox.sendActionsForControlEvents(.TouchUpInside)
-        
-        XCTAssertTrue(cell.checkbox.isChecked, "Checkbox not checked after tap!")
-        waitForExpectationsWithTimeout(2, handler: nil)
     }
 
 }
